515ca8f98bae6e5224e0d552739eb121eca8e7d5
Ivan <77243875+ukra174@users.noreply.github.com>
2023-06-20 01:55:16 +0200

    Added userscript.

---
+++ Multi_language_prompt_on_pages.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         Multi language script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Open a small window with a word or a phrase in multiple languages!
+// @author       ukra174
+// @match        https://*/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var lines = [];
+    const textElement = document.createElement('div');// Create a new element for the text
+    console.log("AaaAAAaaaaAaA");
+
+    fetch('https://ukra174.github.io/Multiple-language-study/words.txt')
+        .then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error('Error: ' + response.status);
+        }
+    })
+        .then(data => {
+        lines = data.split("\n");
+        var words = lines[Math.ceil(Math.random()*lines.length)-1].split("/");
+        document.body.appendChild(textElement);
+        var spanCode = "<span style='color:rgb(255, 85, 187)'>";
+        spanCode+=words[0] + "</span>/<span style='color:rgb(230, 230, 210)'>";
+        spanCode+=words[1] + "</span>/<span style='color:rgb(252, 255, 178)'>";
+        spanCode+=words[2] + "</span>/<span style='color:rgb(182, 234, 250)'>";
+        spanCode+=words[3] + "</span><span style='color:rgb(255, 255, 255)'>";
+        spanCode+=words[4] + "</span>";
+        textElement.innerHTML = spanCode;
+        setTimeout(() => {
+            fadeOut(textElement, 2000);
+        }, 0);
+
+    })
+        .catch(error => {
+        console.error('Error:', error);
+    });
+    // Apply styles for absolute positioning
+    textElement.style.position = 'absolute';
+    textElement.style.top = '10px';
+    textElement.style.left = '10px';
+    textElement.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    textElement.style.color = 'white';
+    textElement.style.padding = '5px';
+    textElement.style.zIndex = '999999';
+    textElement.style.borderRadius = '5px';
+    textElement.style.borderWidth = '1px';
+    textElement.style.borderStyle = 'solid';
+    textElement.style.borderColor = 'rgba(80,80,80,0.5)';
+    textElement.style.pointerEvents = 'none'
+    // Function to make the text fade out gradually
+    function fadeOut(element, duration) {
+        let opacity = 6;
+        const interval = 10;
+        const steps = duration / interval;
+
+        const fadeOutInterval = setInterval(() => {
+
+            opacity -= 1 / steps;
+            element.style.opacity = Math.min(opacity,1);
+
+            if (opacity <= 0) {
+                clearInterval(fadeOutInterval);
+                element.parentNode.removeChild(element);
+            }
+        }, interval);
+    }
+})();
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Multiple-language-study
+
+For use you need Tampermonkey extension for your browser installed.
+
+Just import the *Multi_language_prompt_on_pages.user.js* into it, 
+and script should start working.
+
+
+If you don't want it to work an (almost)all urls, you can change *@match* string manually:
+```
+// @version      0.1
+// @description  Open a small window with a word or a phrase in multiple languages!
+// @author       ukra174
+// @match        your_url_match_string_here
+// ==/UserScript==
+```
